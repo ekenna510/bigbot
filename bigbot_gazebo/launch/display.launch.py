1fe65c193476893dcfd5b257f12fb98ea2044334
ekenna510 <ekenna510@verizon.net>
2023-05-28 12:54:06 -0400

    2 launch and another bot xacro

--- bigbot_gazebo/launch/display.launch.py
+++ bigbot_gazebo/launch/display.launch.py
@@ -1,36 +1,30 @@
 import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
-import xacro
 import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='bigbot_description').find('bigbot_description')
-    print(pkg_share)
-    default_model_path = os.path.join(pkg_share, 'urdf/robot7.urdf')
+    default_model_path = os.path.join(pkg_share, 'urdf/robot6.urdf')
     print(default_model_path)
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     print(default_rviz_config_path )
     with open(default_model_path, 'r') as infp:
         robot_desc = infp.read()
+    pkg_share2 =launch_ros.substitutions.FindPackageShare(package='bigbot_gazebo').find('bigbot_gazebo')
+    world_path=os.path.join(pkg_share2, 'world/my_world.sdf')
+    print(world_path )
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description':  robot_desc}]
     )
-    #Command(['xacro ', LaunchConfiguration('model')])
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -38,6 +32,19 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+    spawn_entity = launch_ros.actions.Node(
+    	package='gazebo_ros', 
+    	executable='spawn_entity.py',
+        arguments=['-entity', 'sam_bot', '-topic', 'robot_description'],
+        output='screen'
+    )
+    robot_localization_node = launch_ros.actions.Node(
+         package='robot_localization',
+         executable='ekf_node',
+         name='ekf_filter_node',
+         output='screen',
+         parameters=[os.path.join(pkg_share2, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
     return launch.LaunchDescription([
@@ -47,8 +54,12 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
+        spawn_entity,
+        robot_localization_node,
         rviz_node
     ])
